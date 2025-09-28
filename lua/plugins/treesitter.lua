0c4b5df8cd977be1264f087e7baa7ec8fc1f1eae
nagarajankarthik <nkarthik3092@gmail.com>
2025-09-28 17:13:42 +0800

    Finalized tree-sitter configuration.

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+        branch = "master",
+        build = ":TSUpdate",
+        event = "BufReadPre",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "c", "cpp", "python",  "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "bash", "tmux" },
+                auto_install = true,
+                highlight = { enable = true },
+                indent = { enable = true },
+        })
+        end
+}
