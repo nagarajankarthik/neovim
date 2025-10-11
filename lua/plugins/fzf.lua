5054ad404a08b23e718b81dfca6c3c90e8db69e6
nagarajankarthik <nkarthik3092@gmail.com>
2025-10-11 10:42:29 +0800

    Modified indentation.

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -2,21 +2,21 @@ return {
 	'junegunn/fzf',
 	'junegunn/fzf.vim',
     config = function()
-	-- Configure fzf actions
-	vim.g.fzf_action = {
-		['ctrl-t'] = 'tab split',
-		['ctrl-x'] = 'split',
-		['ctrl-s'] = 'vsplit',
-		['ctrl-o'] = 'v:lua.fzf_copy_results',
-	}
+        -- Configure fzf actions
+        vim.g.fzf_action = {
+            ['ctrl-t'] = 'tab split',
+            ['ctrl-x'] = 'split',
+            ['ctrl-s'] = 'vsplit',
+            ['ctrl-o'] = 'v:lua.fzf_copy_results',
+        }
 
-	vim.cmd([[
-	let $FZF_DEFAULT_OPTS="--preview-window 'right:57%' --preview 'bat --style=numbers --line-range :300 {}'
-	\ --bind ctrl-y:preview-up,ctrl-e:preview-down,
-	\ctrl-b:preview-page-up,ctrl-f:preview-page-down,
-	\ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down,
-	\shift-up:preview-top,shift-down:preview-bottom,
-	\alt-up:half-page-up,alt-down:half-page-down"
-	]])
+        vim.cmd([[
+        let $FZF_DEFAULT_OPTS="--preview-window 'right:57%' --preview 'bat --style=numbers --line-range :300 {}'
+        \ --bind ctrl-y:preview-up,ctrl-e:preview-down,
+        \ctrl-b:preview-page-up,ctrl-f:preview-page-down,
+        \ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down,
+        \shift-up:preview-top,shift-down:preview-bottom,
+        \alt-up:half-page-up,alt-down:half-page-down"
+        ]])
     end,
 }
