fad11753945e71a6028781f6426eefd34d210abd
nagarajankarthik <nkarthik3092@gmail.com>
2025-10-11 10:42:57 +0800

    Adding fzf options here because it doesn't work when added to the plugins config file.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -43,3 +43,21 @@ vim.diagnostic.config({
         update_in_insert = false,
         severity_sort = true,
 })
+
+
+vim.g.fzf_action = {
+    ['ctrl-t'] = 'tab split',
+    ['ctrl-x'] = 'split',
+    ['ctrl-s'] = 'vsplit',
+    ['ctrl-o'] = 'v:lua.fzf_copy_results',
+}
+
+
+vim.cmd([[
+let $FZF_DEFAULT_OPTS="--preview-window 'right:57%' --preview 'bat --style=numbers --line-range :300 {}'
+\ --bind ctrl-y:preview-up,ctrl-e:preview-down,
+\ctrl-b:preview-page-up,ctrl-f:preview-page-down,
+\ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down,
+\shift-up:preview-top,shift-down:preview-bottom,
+\alt-up:half-page-up,alt-down:half-page-down"
+]])
