94e768a2d93e94324baf7a49b8dd8f0cafac6903
nagarajankarthik <nkarthik3092@gmail.com>
2025-10-19 09:45:48 +0800

    Added lsp to list of completion sources for blink.cmp

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -39,7 +39,7 @@ return {
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'path', 'snippets', 'buffer', 'omni', 'cmdline' },
+      default = { 'lsp', 'path', 'snippets', 'buffer', 'omni', 'cmdline' },
     },
     
     signature = { enabled = true },
